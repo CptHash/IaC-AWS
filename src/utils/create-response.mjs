0fb0c4d2b494e9918a1a0454579985cf5ea181ed
Mathieu <mathieu.houlle@epitech.eu>
2023-01-01 19:21:23 +0100

    Adding cors header in all response

--- src/utils/create-response.mjs
+++ src/utils/create-response.mjs
@@ -4,6 +4,11 @@ export function createResponse(statusCode, body, event) {
 
   return {
     statusCode: statusCode,
+	headers: {
+		"Access-Control-Allow-Headers" : "Content-Type",
+		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT"
+	},
     body: JSON.stringify(body)
   };
 }
